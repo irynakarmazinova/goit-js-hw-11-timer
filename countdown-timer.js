7fe0872fc019ce477907df0bf79c95a75e172c7c
irynakarmazinova <i.karmazinova1992@gmail.com>
2021-07-31 16:48:48 +0300

    made a timer on video

--- countdown-timer.js
+++ countdown-timer.js
@@ -1,7 +1,42 @@
 // таймер обратного отсчета
-class CountdownTimer {}
+
+const mainDivRef = document.querySelector(".timer");
+
+class CountdownTimer {
+  constructor() {}
+
+  start() {}
+
+  stop() {}
+
+  getTimeComponents(time) {
+    const days = this.pad(Math.floor(time / (1000 * 60 * 60 * 24)));
+    const hours = this.pad(
+      Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
+    );
+    const mins = this.pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
+    const secs = this.pad(Math.floor((time % (1000 * 60)) / 1000));
+
+    return { days, hours, mins, secs };
+  }
+
+  pad(value) {
+    return String(value).padStart(2, "0");
+  }
+}
 
 new CountdownTimer({
   selector: "#timer-1",
-  targetDate: new Date("Jul 17, 2019"),
+  targetDate: new Date("August 08, 2021"),
 });
+
+// selector - селектор плагина
+// targetDate - конечная дата, до которого будет распродажа
+
+// отсчет до распродажи
+// когда будет вызываться таймер, класс ваш, то вы будете передавать время до которого будет распродажа, а потом будет от туда считать вниз
+// будет конечное время и текущее и надо будет выбрать разницу между конечным и текущим
+
+function updateClockface({ days, hours, mins, secs }) {
+  mainDivRef.textContent = `${days}:${hours}:${mins}:${secs}`;
+}
--- timer.js
+++ timer.js
@@ -1,47 +1,79 @@
 // таймер отсчет вперед
 const startBtn = document.querySelector("button[data-action-start]");
 const stopBtn = document.querySelector("button[data-action-stop]");
-const clockface = document.getElementById("js-clockface");
+const clockface = document.querySelector(".js-clockface");
 
-const timer = {
+class Timer {
+  constructor({ onTick }) {
+    this.intervalId = null;
+    this.isActive = false;
+    this.onTick = onTick;
+
+    this.init();
+  }
+
+  init() {
+    const time = this.getTimeComponents(0);
+    this.onTick(time);
+  }
+
   start() {
-    const startTime = Date.now();
+    // если мы запускаем таймер и он уже активный, то return из этого кода и больше ничего не выполняй
+    // а если же он не активен, то мы делаем его активным, ставим буль(this.isActive = true) и запускаем setInterval
+    if (this.isActive) {
+      console.log("таймер запущен, кронка старт не активна");
+      return;
+    }
 
-    setInterval(() => {
+    const startTime = Date.now();
+    this.isActive = true;
+
+    this.intervalId = setInterval(() => {
       const currentTime = Date.now();
       const deltaTime = currentTime - startTime;
-      const { days, hours, mins, secs } = getTimeComponents(deltaTime);
+      const time = this.getTimeComponents(deltaTime);
 
-      console.log(`${days}:${hours}:${mins}:${secs}`);
+      this.onTick(time);
     }, 1000);
-  },
-};
-timer.start();
+  }
 
-// принимает врмея в милисекундах
+  stop() {
+    clearInterval(this.intervalId);
+    this.isActive = false;
+    const time = this.getTimeComponents(0);
+    this.onTick(time);
+  }
+
+  // принимает время в милисекундах
+  // высчитывает сколько в них вмещается часов/минут/секунд
+  // возвращает объект со свойствами days, hours, mins, secs
+  // формулы скопированы с стаоверфлоу
+  getTimeComponents(time) {
+    const days = this.pad(Math.floor(time / (1000 * 60 * 60 * 24)));
+    const hours = this.pad(
+      Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
+    );
+    const mins = this.pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
+    const secs = this.pad(Math.floor((time % (1000 * 60)) / 1000));
+
+    return { days, hours, mins, secs };
+  }
+
+  // метод pad(написал репета), приводит к строке и добавляет в начало 0, если число меньше 2-х знаков
+  // если 1 -> то вернет 01; 7 -> 07; 12 -> 12
+  pad(value) {
+    return String(value).padStart(2, "0");
+  }
+}
+
+const timer = new Timer({ onTick: updateClockface });
+
+startBtn.addEventListener("click", timer.start.bind(timer));
+stopBtn.addEventListener("click", timer.stop.bind(timer));
+
+// принимает время в милисекундах
 // высчитывает сколько в них вмещается часов/минут/секунд
 // рисует интерфейс
 function updateClockface({ days, hours, mins, secs }) {
   clockface.textContent = `${days}:${hours}:${mins}:${secs}`;
-}
-
-// метод pad(написал репета), приводит к строке и добавляет в начало 0, если число меньше 2-х знаков
-// если 1 -> то вернет 01; 7 -> 07; 12 -> 12
-function pad(value) {
-  return String(value).padStart(2, "0");
-}
-
-// принимает время в милисекундах
-// высчитывает сколько в них вмещается часов/минут/секунд
-// возвращает объект со свойствами days, hours, mins, secs
-// формулы скопированы с стаоверфлоу
-function getTimeComponents(time) {
-  const days = pad(Math.floor(time / (1000 * 60 * 60 * 24)));
-  const hours = pad(
-    Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
-  );
-  const mins = pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
-  const secs = pad(Math.floor((time % (1000 * 60)) / 1000));
-
-  return { days, hours, mins, secs };
 }
